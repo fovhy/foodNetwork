673fae392c50a8170c1a79468abc82e38324796a
Dean He <Dean He>
2016-09-04 10:05:04 -0500

    added javadoc for all stuff

--- src/foodnetwork/serialization/MealTypeTest.java
+++ src/foodnetwork/serialization/MealTypeTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by fovhy on 9/3/2016.
- */
 public class MealTypeTest {
     @Test
     public void testGetRightMealType() {
---
+++ src/test/MessageInputTest.java
@@ -0,0 +1,9 @@
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fovhy on 9/3/2016.
+ */
+public class MessageInputTest {
+}
