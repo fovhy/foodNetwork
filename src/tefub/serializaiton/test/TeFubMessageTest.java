96dc992f1ec4d2663ceaf4f2ea6a6f2b588d81d9
Dean He <Dean He>
2016-10-19 10:25:27 -0500

    added java doc for Tefub Message test

--- src/tefub/serializaiton/test/TeFubMessageTest.java
+++ src/tefub/serializaiton/test/TeFubMessageTest.java
@@ -30,6 +30,11 @@ public abstract class TeFubMessageTest {
     protected long expMsgId;          // expected message ID
     protected int expCode;           // expected messageCode
     public static final String CHARSET = "ASCII"; // default encoding
+
+    /**
+     * The data for parameterized test
+     * @return
+     */
     @Parameterized.Parameters
     public static Collection<Object[]> data(){
         ArrayList testList = new ArrayList();
@@ -54,28 +59,66 @@ public abstract class TeFubMessageTest {
 
         return testList;
     }
+
+    /**
+     * The constructor for the TeFubMessage test. Equals to setup
+     * @param expSerialization
+     * @param expMsgId
+     * @param expCode
+     */
     public TeFubMessageTest(byte[] expSerialization, int expMsgId, int expCode){
         this.expSerialization = expSerialization;
         this.expMsgId = expMsgId;
         this.expCode = expCode;
     }
+
+    /**
+     * The method for sub classes to very their expected message, or byte array
+     * in this case
+     * @param teFubMessage
+     */
     public abstract void verifyExpectedMessage(TeFubMessage teFubMessage);
     public abstract TeFubMessage getDeserializeMessage()throws IllegalArgumentException, IOException;
     protected static byte[] binaryStringToByteArray(String binaryString){
         return new BigInteger(binaryString, 2).toByteArray();
     }
+
+    /**
+     * Concat two byte stream
+     * @param first the first byte array to combine
+     * @param second the second byte array to combine
+     * @return the combined stream
+     * @throws IOException if the write to byteStream fails
+     */
     public static byte[] concat(byte[] first, byte[] second) throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
         outputStream.write(first);
         outputStream.write(second);
         return outputStream.toByteArray( );
     }
+
+    /**
+     * Generic function for combine two
+     * @param first the first generic array
+     * @param second the second generic array
+     * @param <T> the data type of the array
+     * @return the combined array
+     */
     protected static <T> T[] concat(T[] first, T[] second){
         // concat two generic array
         T[] toReturn = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, toReturn, first.length, second.length);
         return toReturn;
     }
+
+    /**
+     * Combine two collection array, here it is used to combine the test data
+     * of base class and derived classes
+     * @param base the base class test data
+     * @param sub the sub class test data
+     * @return the combined test data
+     * @throws IOException if the system fails to copy the array
+     */
     protected static Object[] buildTest(Object[] base, Object[] sub) throws IOException {
         byte[] expBytes = concat((byte[])base[0],(byte[])sub[0]);
         return concat(new Object[]{expBytes},
@@ -85,20 +128,41 @@ public abstract class TeFubMessageTest {
     }
 
 
+    /**
+     * Verify the base class data of base class
+     * @param teFubMessage the teFubMessage to be tested
+     * @param expMsgId the expected message id
+     * @param expCode the expected code
+     */
     public void verifyBaseMessage(TeFubMessage teFubMessage, int expMsgId, int expCode){
         assertEquals(expMsgId, teFubMessage.getMsgId());
         assertEquals(expMsgId, teFubMessage.getCode());
     }
+
+    /**
+     * Test the decode function for TeFubMessage
+     * @throws IOException if it fails to decode
+     */
     @Test
     public void testDecode() throws IOException {
         TeFubMessage message = TeFubMessage.decode(this.expSerialization);
         this.verifyExpectedMessage(message);
     }
+
+    /**
+     * Test the encode function for the TeFubMessage
+     * @throws IOException if the encode fails
+     */
     @Test
     public void testEncode() throws IOException {
         TeFubMessage message = this.getDeserializeMessage();
         assertEquals(expSerialization, message.encode());
     }
+
+    /**
+     * Test the if the decode fails if there is not enough bytes
+     * @throws IOException expected
+     */
     @Test(expected = IOException.class)
     public void testShortDecode() throws IOException {
         byte[] shortArray = Arrays.copyOfRange(this.expSerialization, 0, this.expSerialization.length - 1);
