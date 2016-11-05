32a6347f6008f09e45fc35ea8a6aa2fcdf98962c
Yume <Dean>
2016-11-05 08:05:32 -0500

    sync

--- src/tefub/server/TeFubServer.java
+++ src/tefub/server/TeFubServer.java
@@ -9,7 +9,6 @@ package tefub.server;
 
 import tefub.serialization.TeFubMessage;
 
-import java.io.IOException;
 import java.net.*;
 
 import static java.lang.Thread.sleep;
@@ -18,14 +17,40 @@ import static java.lang.Thread.sleep;
  * For now it is just an echo server sort of
  */
 public class TeFubServer extends Thread{
-    private int portNumber;
+    private int portNumber; // the port number for UDP socket, should be the same as the foodNetwork one
+    private static DatagramSocket sock; // the server data socket
     TeFubServer(int portNumber){
         this.portNumber = portNumber;
     }
-    private void constructSocket(){
 
+    /**
+     * The setup of the connection of the UDP socket
+     */
+    private void constructSocket(){
+        try {
+            checkPort(this.portNumber);
+        }catch(IllegalArgumentException e){
+            //TODO: figure out whether to send a message or not
+            // not even sure this is needed, it could be handled by the socket exception
+        }
+        try {
+            sock = new DatagramSocket(portNumber);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
     }
-    private void receiveAndResponse(){
+
+    /**
+     * Check if the port is out of range
+     * @param portNumber the port number you wish to check
+     */
+    private void checkPort(int portNumber) throws IllegalArgumentException{
+    }
+
+    /**
+     * The process of waiting to receive, and respond to the recevied message
+     */
+    private void receiveAndRespond(){
         TeFubMessage receviedMessage = receiveData();
     }
     private void respond(TeFubMessage message){
@@ -34,20 +59,26 @@ public class TeFubServer extends Thread{
 
     /**
      * Send a certain TeFubMessage to the client
-     * @param message
+     * @param message the message you want to send out to all the clients registered
      */
     private void sendMessage(TeFubMessage message){
 
     }
+
+    /**
+     * Receive data, and transfer the data into TeFubMessage if something bad happen,
+     * it throws. (i.e. parsing exception, wrong code, unexpected code)
+     * @return the message you want to return
+     */
     private TeFubMessage receiveData(){
         return null;
     }
+
+    /**
+     *The method it inherits from thread. Basically the main for the class
+     */
     public void run(){
         constructSocket();
-        receiveAndResponse();
-    }
-    private static DatagramSocket sock;
-    public static void main(String[] args) throws IOException {
-
+        receiveAndRespond();
     }
 }
